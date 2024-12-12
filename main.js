5fc22ae2c5102d5867b226a511132a917963b665
jacey111 <161446475+jacey111@users.noreply.github.com>
2024-12-12 12:13:07 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function showAbout() {
+    document.getElementById("about").style.display = "block";
+}
+
+function hideAbout() {
+    document.getElementById("about").style.display = "none";
+}
+
+function showGallery() {
+    document.getElementById("gallery").style.display = "block";
+}
+
+function hideGallery() {
+    document.getElementById("gallery").style.display = "none";
+}
+
+function showAchievement() {
+    document.getElementById("achievement").style.display = "block";
+}
+
+function hideAchievement() {
+    document.getElementById("achievement").style.display = "none";
+}
