b4bf8a6a161f4fec66303e287663ddb0d80cf03e
jacey111 <161446475+jacey111@users.noreply.github.com>
2024-12-18 14:13:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function showAbout() {
     aboutDiv.style.display = "block";
     aboutDiv.style.opacity = "1";
 
-    smoothScrollTo(aboutDiv, 100);
+    smoothScrollTo(aboutDiv, 300);
 }
 
 function hideAbout() {
@@ -37,7 +37,7 @@ function hideAbout() {
     aboutDiv.style.opacity = "0";
     aboutDiv.style.display = "none";
 
-    smoothScrollTo(document.getElementById("overlay-about"), 100);
+    smoothScrollTo(document.getElementById("overlay-about"), 300);
 }
 
 function showGallery() {
@@ -46,7 +46,7 @@ function showGallery() {
     galleryDiv.style.display = "block";
     galleryDiv.style.opacity = "1";
 
-    smoothScrollTo(galleryDiv, 100);
+    smoothScrollTo(galleryDiv, 300);
 }
 
 function hideGallery() {
@@ -55,7 +55,7 @@ function hideGallery() {
     galleryDiv.style.opacity = "0";
     galleryDiv.style.display = "none";
 
-    smoothScrollTo(document.getElementById("overlay-gallery"), 100);
+    smoothScrollTo(document.getElementById("overlay-gallery"), 300);
 }
 
 function showAchievement() {
@@ -64,7 +64,7 @@ function showAchievement() {
     achievementDiv.style.display = "block";
     achievementDiv.style.opacity = "1";
 
-    smoothScrollTo(achievementDiv, 100);
+    smoothScrollTo(achievementDiv, 300);
 }
 
 function hideAchievement() {
@@ -73,5 +73,5 @@ function hideAchievement() {
     achievementDiv.style.opacity = "0";
     achievementDiv.style.display = "none";
 
-    smoothScrollTo(document.getElementById("overlay-achievement"), 100);
+    smoothScrollTo(document.getElementById("overlay-achievement"), 300);
 }
